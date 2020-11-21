d80b5c05d4cb1f5a39f454295b083e3887840dbe
yyjj745 <yyjj745@gmail.com>
2020-11-22 05:12:52 +0900

    background-search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import { Component } from 'react';
 import './App.css';
-
-
-
-class Header extends Component {
-  render() {
-    return (
-      <h1>{this.props.title}</h1>
-    );
-  }
-}
+import './style.css';
+import styled from "styled-components";
+// import Search from "./component/Search";
 
 class App extends Component {
+  state = {
+    query: ""
+  };
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Background query={this.state.query}>
+        <input class="search" placeholder="테마를 입력하세요" onKeyPress={this.handleKeyPress}></input>
+      </Background>
     );
   }
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.setState({
+        query: event.target.value
+      });
+      event.target.value = "";
+    }
+  }
 }
+
+const Background = styled.div`
+  position:absolute;
+  z-index:-1;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-size: cover;
+  background: linear-gradient(
+    to right,
+    rgba(20, 20, 20, 0.1) 10%,
+    rgba(20, 20, 20, 0.7) 70%,
+    rgba(20, 20, 20, 1)
+    ),
+    url(https://source.unsplash.com/random/1920x1080?${props => props.query});
+`;
 
 export default App;
